73d4a41e585a1ef7dfa5fa40857f58891ba30fbe
eduardo <horstmanneduardo100@gmail.com>
2025-06-07 02:05:44 +0400

    refactor: remove unused import and unnecessary public modifiers in CheckinCheckoutTest

--- src/test/java/com/demo/travelcardsystem/CheckinCheckoutTest.java
+++ src/test/java/com/demo/travelcardsystem/CheckinCheckoutTest.java
@@ -4,7 +4,6 @@ import com.demo.travelcardsystem.constant.TransportType;
 import com.demo.travelcardsystem.entity.TravelCard;
 import com.demo.travelcardsystem.model.request.SwipeRequest;
 import com.demo.travelcardsystem.repository.InMemoryCardTransactionRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class CheckinCheckoutTest extends IntegrationTest {
+class CheckinCheckoutTest extends IntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,13 +29,13 @@ public class CheckinCheckoutTest extends IntegrationTest {
 
 
     @BeforeEach
-    public void resetRepository() {
+    void resetRepository() {
         inMemoryCardTransactionRepository.clearTravelCardStore();
     }
 
     @DisplayName("User take few trips and check balance at end of the trip")
     @Test
-    public void user_take_trip_and_check_balance() throws Exception {
+    void user_take_trip_and_check_balance() throws Exception {
         //GIVEN - User/Travel-card exists in the system
         TravelCard travelCard = travelHelperTest.directUserRegistration("1A101", 30);
         SwipeRequest swipeRequest =  null;
